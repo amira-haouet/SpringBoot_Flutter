69f280ac6c7216451d77680c6268edeab988881f
Amira Haouet <haouetamira@gmail.com>
2021-11-27 12:26:01 +0100

    sprint init commit

---
+++ backend_Spring/scolarite_springboot_/src/main/java/com/example/demo/entities/Classe.java
@@ -0,0 +1,21 @@
+package com.example.demo.entities;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import javax.persistence.*;
+import java.util.Collection;
+
+@Entity
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+public class Classe {
+	@Id @GeneratedValue(strategy=GenerationType.IDENTITY)
+	private Long codClass;
+	private String nomClass;
+	private int nbreEtud;
+	@OneToMany(mappedBy="classe")
+	private Collection<Etudiant> etudiants; 
+}
